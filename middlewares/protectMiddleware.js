bad5eee18eaac783db3163f0b93308cdf1ad2bba
saykat1 <nursaykat@gmail.com>
2022-01-12 19:51:19 +0600

    protect fix

--- middlewares/protectMiddleware.js
+++ middlewares/protectMiddleware.js
@@ -7,7 +7,7 @@ const protect = async (req, res, next) => {
 
     if (
       req.headers.authorization &&
-      req.headers.authorization.startsWidth("Bearer")
+      req.headers.authorization.startsWith("Bearer")
     ) {
       try {
         token = req.headers.authorization.split(" ")[1];
